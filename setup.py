993dbb0e83255a60a97c717710a6b20585cdb9e6
suyog-kulkarni-pst <suyog@positsource.com>
2025-01-20 11:13:51 +0530

    Released new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py_error_boundary",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple Python package for explicit errors",
     packages=find_packages(),
     author="Suyog Kulkarni",
